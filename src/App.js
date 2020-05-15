98b0af62feb3e6c3e86535fc7cfe2e05361e5e2d
AYUSHSUDHAKAR <ayushsudhakar1998@gmail.com>
2020-05-15 22:09:51 +0530

    props and eventhandling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
     <div className="App">
@@ -12,6 +22,7 @@ class App extends Component {
           <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={this.state.dishes} />
     </div>
   );
   }
